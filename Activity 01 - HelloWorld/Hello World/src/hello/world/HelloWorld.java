180f9931591256d56e85ef5d3aba7d2877b778b9
Thiago Viotto <thiagohenriqueviotto1@gmail.com>
2017-08-09 16:26:00 -0300

    Versao atualizada

---
+++ Activity 01 - HelloWorld/Hello World/src/hello/world/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * Disciplina: Programação Orientada Objetos 2 
+ * Prof.Dr.Ivan Ricarte
+ * Curso: Tec.Análise e Desenvolvimento de Sistemas
+ */
+package hello.world;
+
+/**
+ *
+ * @author thiago viotto
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+    
+}
