7144ec89d98deacaad2d6813ee55accfc9cd5ea3
Thiago Viotto <thiagohenriqueviotto1@gmail.com>
2017-08-25 17:52:49 -0300

    Inicio da implementacao das classes

---
+++ ClinicaVeterinaria/src/br/ftunicamp/veterinaria/model/Animal.java
@@ -0,0 +1,40 @@
+/*
+ * Disciplina: Programação Orientada Objetos 2 
+ * Prof.Dr.Ivan Ricarte
+ * Curso: Tec.Análise e Desenvolvimento de Sistemas
+ */
+
+
+package br.ftunicamp.veterinaria.model;
+/**
+ *
+ * @author Thiago Henrique Viotto
+ */
+public class Animal { //Classe com dados dos animais
+    private final String nome;
+    private String idade;
+    private char sexo;
+   
+    //cadastro de um animal
+    public Animal(String nome, String idade, char sexo) { 
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+
+    //retorna o nome do animal
+    public String getNome() {
+        return nome;
+    }
+
+    //retorna a idade do animal
+    public String getIdade() {
+        return idade;
+    }
+
+    //retorna o sexo do animal
+    public char getSexo() {
+        return sexo;
+    }
+
+}
---
+++ ClinicaVeterinaria/src/br/ftunicamp/veterinaria/model/Especie.java
@@ -0,0 +1,27 @@
+/*
+ * Disciplina: Programação Orientada Objetos 2 
+ * Prof.Dr.Ivan Ricarte
+ * Curso: Tec.Análise e Desenvolvimento de Sistemas
+ */
+
+package br.ftunicamp.veterinaria.model;
+
+/**
+ *
+ * @author Thiago Henrique Viotto
+ */
+public class Especie { //Classe Especie de animal
+    private final String nome;
+
+    //cadastro de uma especie
+    public Especie(String nome) {
+        this.nome = nome;
+    }
+
+    //retorna o nome da especie
+    public String getNome() {
+        return nome;
+    }
+    
+    
+}
