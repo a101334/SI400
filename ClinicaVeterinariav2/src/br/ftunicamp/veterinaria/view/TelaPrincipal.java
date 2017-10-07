c6fd1491f0b3441213ce325c352fcf2a5cd548f2
a101334 <alex.101334@hotmail.com>
2017-10-07 10:04:31 -0300

    Criamos a Tela de login e a Tela principal

--- ClinicaVeterinariav2/src/br/ftunicamp/veterinaria/view/TelaPrincipal.java
+++ ClinicaVeterinariav2/src/br/ftunicamp/veterinaria/view/TelaPrincipal.java
@@ -27,7 +27,46 @@ public class TelaPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenuBar1 = new javax.swing.JMenuBar();
+        menuArquivo = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        menuAgenda = new javax.swing.JMenu();
+        menuUsuarios = new javax.swing.JMenu();
+        menuClientes = new javax.swing.JMenu();
+        menuAnimais = new javax.swing.JMenu();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jMenuBar1.setBackground(new java.awt.Color(82, 58, 21));
+        jMenuBar1.setForeground(new java.awt.Color(245, 175, 11));
+        jMenuBar1.setFont(new java.awt.Font("Ubuntu", 1, 15)); // NOI18N
+
+        menuArquivo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/source/Pacote de Icones/icons 1/application_xp.png"))); // NOI18N
+        menuArquivo.setText("Arquivo");
+
+        jMenuItem1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/source/Pacote de Icones/icons 1/stop.png"))); // NOI18N
+        jMenuItem1.setText("Sair");
+        menuArquivo.add(jMenuItem1);
+
+        jMenuBar1.add(menuArquivo);
+
+        menuAgenda.setIcon(new javax.swing.ImageIcon(getClass().getResource("/source/Pacote de Icones/icons 1/calendar.png"))); // NOI18N
+        menuAgenda.setText("Agenda");
+        jMenuBar1.add(menuAgenda);
+
+        menuUsuarios.setIcon(new javax.swing.ImageIcon(getClass().getResource("/source/Pacote de Icones/icons 1/user.png"))); // NOI18N
+        menuUsuarios.setText("Usuarios");
+        jMenuBar1.add(menuUsuarios);
+
+        menuClientes.setIcon(new javax.swing.ImageIcon(getClass().getResource("/source/Pacote de Icones/icons 1/group.png"))); // NOI18N
+        menuClientes.setText("Clientes");
+        jMenuBar1.add(menuClientes);
+
+        menuAnimais.setIcon(new javax.swing.ImageIcon(getClass().getResource("/source/Pacote de Icones/icons 1/tux.png"))); // NOI18N
+        menuAnimais.setText("Animais");
+        jMenuBar1.add(menuAnimais);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -37,7 +76,7 @@ public class TelaPrincipal extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 279, Short.MAX_VALUE)
         );
 
         pack();
@@ -79,5 +118,12 @@ public class TelaPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenu menuAgenda;
+    private javax.swing.JMenu menuAnimais;
+    private javax.swing.JMenu menuArquivo;
+    private javax.swing.JMenu menuClientes;
+    private javax.swing.JMenu menuUsuarios;
     // End of variables declaration//GEN-END:variables
 }
